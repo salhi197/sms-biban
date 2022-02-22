3a665899d6ff0a8454642b9d8475f22739742f79
Haider Ali salhi <salhiali197@gmail.com>
2022-02-22 14:59:04 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000;
+var request=require('request');
+const axios = require('axios');
+var timeout = require('connect-timeout')
+
+var bodyParser = require('body-parser')
+
+app.use(timeout('10s'))
+
+function send_sms(telephone,message,res){
+  var url = 'http://sms.icosnet.com:8080/bulksms/bulksms?username=BIBAN_FRET&password=SMS3265&type=0&dlr=1&destination=213794498727&source=BIBAN%20FRET&message='+message
+  axios.get(url)
+  .then(function (response) {
+    // handle success
+    res.send("working")
+  })
+  .catch(function (error) {
+    // handle error
+    res.send("error")
+  })
+}
+app.listen(port);
+app.get('/sms', timeout('5s'),bodyParser.json(),haltOnTimedout,function(params,res){
+  var telephone = params.query.telephone
+  var message = params.query.message
+  send_sms(telephone,message,res)
+  
+});
+function haltOnTimedout (req, res, next) {
+  if (!req.timedout) next()
+}
+app.get('/', function(req, res) {
+  res.send("working")
+})
+
